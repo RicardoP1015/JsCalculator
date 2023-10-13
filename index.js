43b125a12853c6821d2b6f523f81e88aca592255
RicardoP <picoricardo3d@outlook.com>
2023-10-13 00:51:46 -0400

    Made a function to handle what Math operation is used
    the new function handleMathOp will take a perimeter then depending
    on it will use one of the other four math functions

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
+const numberButtons = document.querySelectorAll('.num-btn');
+const divideSymbol = document.getElementById('/');
+const multiplySymbol = document.getElementById('*');
+const subtractionSymbol = document.getElementById('-');
+const additionSymbol = document.getElementById('+');
+
+const calDisplay = document.querySelector('.cal-display')
+let firstNumber = null;
+let secondNumber = null;
+
 const add = (num1, num2) =>  num1 + num2;
 const sub =(num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
+
+const handleMathOp = (op) => {
+    switch (op) {
+        case '+':
+            return add(firstNumber, secondNumber);
+        case '-':
+            return sub(firstNumber, secondNumber);
+        case '*':
+            return multiply(firstNumber, secondNumber);
+        case '/':
+            return divide(firstNumber, secondNumber);
+    };
+};
+
